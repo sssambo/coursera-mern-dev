0e07636fcb27da1d3b5f28adeeea535508d5275e
sambosaidu <sambosaidu887@gmail.com>
2025-02-19 04:46:26 +0100

    init setup, server file, user model, routes, registration routes in user 2

---
+++ routes/api/user.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../../model/User");
+const gravatar = require("gravatar");
+const bcrypt = require("bcrypt");
+
+router.get("/test", (req, res) => {
+  console.log(req.url);
+  res.json({ message: "GET api/user route" });
+});
+
+router.post("/register", (req, res) => {
+  console.log(req.body);
+  /*
+  console.log(req.body);
+  return res.status(400).json({ message: `maintenance` }); */
+  let { name, email, password, avatar } = req.body;
+
+  User.findOne({ email: email }).then((user) => {
+    if (user) {
+      return res.status(400).json({ message: `User ${email} already exist` });
+    } else {
+      avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      const newUser = new User({
+        name: name,
+        email: email,
+        password: password,
+        avatar: avatar,
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => {
+              return res.json({ user: `user ${user} created successfully` });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        });
+      });
+    }
+  });
+});
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const post = require("./routes/api/post");
+const profile = require("./routes/api/profile");
+const user = require("./routes/api/user");
+app.use(express.urlencoded({ extended: false }));
+//app.use(express.json());
+app.use("/api/post", post);
+app.use("/api/profile", profile);
+app.use("/api/user", user);
+
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
